bf9d36bba09b7f036f5b2a94cf1c64f790775c23
OB4413 <lionousama4@gmail.com>
2025-02-05 10:22:18 +0100

    hh

--- src/check_path.c
+++ src/check_path.c
@@ -6,7 +6,7 @@
 /*   By: obarais <obarais@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 21:58:21 by obarais           #+#    #+#             */
-/*   Updated: 2025/01/26 17:29:04 by obarais          ###   ########.fr       */
+/*   Updated: 2025/02/05 10:18:47 by obarais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 void	*ft_check_path(char *path)
 {
 	size_t	i;
+	int		j;
 
 	i = ft_strlen(path);
 	i--;
-	if (path[i] != 'r' || path[i - 1] != 'e' || path[i - 2] != 'b' || path[i
-			- 3] != '.')
+	if (path[0] == '.')
 	{
-		printf("Error: Invalid file extension\n");
+		printf("Error: Invalid file path or extension\n");
 		exit(1);
+	}
+	j = 0;
+	while (path[j])
+	{
+		if (path[j] == '/')
+		{
+			if (path[j + 1] == '.' || (path[i] != 'r' || path[i - 1] != 'e' || path[i - 2] != 'b' || path[i- 3] != '.' || path[i - 4] == '/'))
+			{
+				printf("Error: Invalid file path or extension\n");
+				exit(1);
+			}
+		}
+		j++;
 	}
 	return (NULL);
 }
